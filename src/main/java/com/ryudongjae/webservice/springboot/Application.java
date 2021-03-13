c30db4aa301b5b1aa3c3e6ea2327c444fff27c91
ryudongjae <rdj10149@gmail.com>
2021-03-13 15:41:37 +0900

    Commit

--- src/main/java/com/ryudongjae/webservice/springboot/Application.java
+++ src/main/java/com/ryudongjae/webservice/springboot/Application.java
@@ -2,12 +2,12 @@ package com.ryudongjae.webservice.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing //jpa auditing 활성화
 @SpringBootApplication
 public class Application {
-
 	public static void main(String[] args) {
-
 		SpringApplication.run(Application.class, args);
 	}
 
