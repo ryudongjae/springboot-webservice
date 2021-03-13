95d9d2f85a124da442aebaba1201e7e64b4c2c4d
ryudongjae <rdj10149@gmail.com>
2021-03-13 20:55:51 +0900

    Commit

--- src/main/java/com/ryudongjae/webservice/springboot/Application.java
+++ src/main/java/com/ryudongjae/webservice/springboot/Application.java
@@ -8,6 +8,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @SpringBootApplication
 public class Application {
 	public static void main(String[] args) {
+
 		SpringApplication.run(Application.class, args);
 	}
 
