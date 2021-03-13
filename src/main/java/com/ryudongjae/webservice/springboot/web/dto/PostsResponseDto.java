81074124533e874cd46bf8b7297b045441e2bf59
ryudongjae <rdj10149@gmail.com>
2021-03-13 20:56:10 +0900

    Update

--- src/main/java/com/ryudongjae/webservice/springboot/web/dto/PostsResponseDto.java
+++ src/main/java/com/ryudongjae/webservice/springboot/web/dto/PostsResponseDto.java
@@ -8,14 +8,14 @@ public class PostsResponseDto {
    private Long id;
    private String title;
    private String content;
-   private String auther;
+   private String author;
 
    public PostsResponseDto(Posts entity){
 
        this.id = entity.getId();
        this.title =entity.getTitle();
        this.content =entity.getContent();
-       this.auther =entity.getAuther();
+       this.author =entity.getAuther();
    }
 
 }
