0b13c9eb1aaa284bf43aee498279c88025c46a0d
ryudongjae <rdj10149@gmail.com>
2021-03-13 20:56:29 +0900

    Update

---
+++ src/main/java/com/ryudongjae/webservice/springboot/web/IndexController.java
@@ -0,0 +1,20 @@
+package com.ryudongjae.webservice.springboot.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+
+    @GetMapping("/")
+    public String index(){
+
+        return "index";
+    }
+
+    @GetMapping("/posts/save")
+    public String postsSave(){
+
+        return "posts-save";
+    }
+}
