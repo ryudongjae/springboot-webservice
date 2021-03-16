42340b14db22a63d142bab26f5a04635b84c6428
ryudongjae <rdj10149@gmail.com>
2021-03-16 14:05:51 +0900

    Update

--- src/main/java/com/ryudongjae/webservice/springboot/domain/posts/PostsRepository.java
+++ src/main/java/com/ryudongjae/webservice/springboot/domain/posts/PostsRepository.java
@@ -1,7 +1,12 @@
 package com.ryudongjae.webservice.springboot.domain.posts;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface   PostsRepository extends JpaRepository<Posts, Long> {
+    @Query("SELECT p From Posts p ORDer BY p.id DESC")
+    List<Posts> findAllDesc();
 
 }
